840be040ee89a8aa4857a13ced2acc5ee3238f66
Clarence Jordan <clarencejordan087@gamil.com.com>
2025-01-11 12:20:50 -0600

    add router and reconfigureation

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,8 @@ function App() {
     <main className="w-full">
       <div className="mt-4">
         <Nav />
-
-        <div className="flex ">
-          <hr className="h-[85vh] ml-14 bg-black w-[1px] mt-10" />
-          <div className="w-full mt-36">
-            <Home />
-          </div>
-        </div>
-        <div className="ml-12 flex items-center justify-center w-11/12">
-          <span className="text-2xl text-center mr-1 mb-1">+</span>
-          <hr className="flex-grow bg-black h-[2px] " />
-        </div>
+        <Home/>
+  
       </div>
     </main>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,24 +3,35 @@ import SubComponetOne from "../componets/SubComponetOne";
 import arrow from "../assets/svg/prime_arrow-up.svg";
 const Home = () => {
   return (
-    <div className="flex flex-col items-center w-11/12 chivo">
-      <div className="text-left w-full ml-10">
-        <HeadComponetOne text="CLARENCE JORDAN" />
-      </div>
+    <>
+      <div className="flex ">
+        <hr className="h-[85vh] ml-14 bg-black w-[1px] mt-10" />
+        <div className="w-full mt-36">
+          <div className="flex flex-col items-center w-11/12 chivo">
+            <div className="text-left w-full ml-10">
+              <HeadComponetOne text="CLARENCE JORDAN" />
+            </div>
 
-      <div className="text-right">
-        <HeadComponetOne text="SOFTWARE ENGINEER" />
-        <sub className="text-center">
-          <SubComponetOne text="Building custom web apps for small businesses" />
-        </sub>
+            <div className="text-right">
+              <HeadComponetOne text="SOFTWARE ENGINEER" />
+              <sub className="text-center">
+                <SubComponetOne text="Building custom web apps for small businesses" />
+              </sub>
+            </div>
+            <span className="pt-96 pr-96 mr-96 flex">
+              <img src={arrow} alt="arrow" />
+              <a href="# " className="self-center">
+                REQUEST PROJECT
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
-      <span className="pt-96 pr-96 mr-96 flex">
-        <img src={arrow} alt="arrow" />
-        <a href="# " className="self-center">
-          REQUEST PROJECT
-        </a>
-      </span>
-    </div>
+      <div className="ml-12 flex items-center justify-center w-11/12">
+        <span className="text-2xl text-center mr-1 mb-1">+</span>
+        <hr className="flex-grow bg-black h-[2px] " />
+      </div>
+    </>
   );
 };
 
