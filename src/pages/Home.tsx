c4ddc3713b62553559cf6afcb8e632394251d47c
Clarence Jordan <clarencejordan087@gamil.com.com>
2025-01-10 23:18:47 -0600

    add update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import HeadComponetOne from "../componets/HeadComponetOne";
 import SubComponetOne from "../componets/SubComponetOne";
+import arrow from "../assets/svg/prime_arrow-up.svg";
 const Home = () => {
   return (
-    <div className="flex flex-col items-center w-11/12">
+    <div className="flex flex-col items-center w-11/12 chivo">
       <div className="text-left w-full ml-10">
         <HeadComponetOne text="CLARENCE JORDAN" />
       </div>
@@ -13,6 +14,12 @@ const Home = () => {
           <SubComponetOne text="Building custom web apps for small businesses" />
         </sub>
       </div>
+      <span className="pt-96 pr-96 mr-96 flex">
+        <img src={arrow} alt="arrow" />
+        <a href="# " className="self-center">
+          REQUEST PROJECT
+        </a>
+      </span>
     </div>
   );
 };
