b85520b27fdd6ec1ba5c2a67b0de8ebcd6dcedbd
Clarence Jordan <clarencejordan087@gamil.com.com>
2025-01-29 17:14:28 -0600

    add update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import Home from './Home'
-import About from './About'
-import Projects from './Projects'
-import Mail from '../componets/Mail'
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Mail from "../componets/Mail";
 
 const Index = () => {
   return (
     <>
-    <Home/>
-    <About/>
-    <Projects/>
-    <Mail/>
+      <div className="h-screen w-full  ">
+        <Home />
+      </div>
+      <div className="h-screen w-full ">
+        <About />
+      </div>
+      <div className="h-screen w-full flex items-center">
+        <Projects />
+      </div>
+      <div className="h-screen w-full flex items-center">
+        <Mail />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
