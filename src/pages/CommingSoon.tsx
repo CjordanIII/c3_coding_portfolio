6c45da5442219f53a94f84edd51910a51890cad8
Clarence Jordan <clarencejordan087@gamil.com.com>
2025-02-03 16:47:57 -0600

    add update

--- src/pages/CommingSoon.tsx
+++ src/pages/CommingSoon.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import HeadComponetOne from '../componets/HeadComponetOne'
-import HeaderOne from '../componets/HeaderOne'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import HeadComponetOne from "../componets/HeadComponetOne";
+import HeaderOne from "../componets/HeaderOne";
 
 const CommingSoon = () => {
-  return (
-    <div className='flex flex-col'><HeaderOne head={<HeadComponetOne text='Comming Soon ...'/>}/></div>
-  )
-}
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(5); // Countdown starts from 5 seconds
 
-export default CommingSoon
+  useEffect(() => {
+    // Timer to update countdown every second
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    // Redirect when countdown reaches zero
+    const redirectTimer = setTimeout(() => {
+      navigate("/");
+    }, 5000); // Redirects after 5 seconds
+
+    // Cleanup the timers if the component unmounts
+    return () => {
+      clearInterval(timer);
+      clearTimeout(redirectTimer);
+    };
+  }, [navigate]);
+
+  return (
+    <div className="flex flex-col">
+      <HeaderOne
+        head={<HeadComponetOne text={`Comming Soon ... (${countdown}s)`} />}
+      />
+    </div>
+  );
+};
+
+export default CommingSoon;
